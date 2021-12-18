a0e28b0b7eb24565caf232ba7d1708ced7620847
Jan Van Sweevelt <vansweej@gmail.com>
2021-12-18 13:49:14 +0100

    getting haskell from older LTS

--- haskell/shell.nix
+++ haskell/shell.nix
@@ -1,4 +1,4 @@
-{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-21.05.tar.gz") {} }:
+{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
 
 pkgs.mkShell {
   buildInputs = [
