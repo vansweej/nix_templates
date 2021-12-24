a1864aee2f973d154e927616dd6fcd3e5067b71b
Jan Van Sweevelt <vansweej@gmail.com>
2021-12-24 14:12:44 +0100

    fix

--- typescript/shell.nix
+++ typescript/shell.nix
@@ -1,18 +1,25 @@
 {pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.node2nix
-    pkgs.nodejs-16_x
-    pkgs.bash-language-server
-    pkgs.dockerfile-language-server-nodejs
-    pkgs.eslint
-    pkgs.typescript
-    pkgs.typescript-language-server
-    pkgs.vscode-css-languageserver-bin
-    pkgs.vscode-html-languageserver-bin
-    pkgs.bashInteractive
-  ];
+with pkgs;
+
+mkShell {
+    buildInputs = with pkgs;
+
+    [
+      (with nodePackages;
+      [
+        node2nix
+        nodejs-16_x
+        bash-language-server
+        dockerfile-language-server-nodejs
+        eslint
+        typescript
+        typescript-language-server
+        vscode-css-languageserver-bin
+        vscode-html-languageserver-bin
+        bashInteractive
+      ])
+    ];
 
   shellHook = ''
   '';
